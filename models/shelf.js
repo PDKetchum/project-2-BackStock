807f213535a8c0c2e6e82ab9beecef70ef04ebda
PDKetchum <pxiong265@gmail.com>
2022-10-13 19:21:58 -0500

    Chaged class to uppercase

--- models/shelf.js
+++ models/shelf.js
@@ -1,39 +1,35 @@
 // this table links all tables together to show what is in a location
 
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
-// create shelf table 
-class shelf extends Model {
-
-}
-shelf.init ( {
-ProductID: {
+// create shelf table
+class Shelf extends Model {}
+Shelf.init({
+  ProductID: {
     type: DataTypes.INTEGER,
     allowNull: false,
     references: {
-        model: "Product", 
-        key: 'id'
-    }
-},
-Stocked_By: {
+      model: "Product",
+      key: "id",
+    },
+  },
+  Stocked_By: {
     type: DataTypes.INTEGER,
-    allowNull: false, 
+    allowNull: false,
     references: {
-        model: "User", 
-        key: 'id'
-    }
-
-},
-LocationID: {
+      model: "User",
+      key: "id",
+    },
+  },
+  LocationID: {
     type: DataTypes.INTEGER,
-    allowNull: false, 
+    allowNull: false,
     references: {
-        model: "Locations", 
-        key: 'id'
-    }
-}
-
-})
-Model.exports =shelf; 
+      model: "Locations",
+      key: "id",
+    },
+  },
+});
+Model.exports = Shelf;
