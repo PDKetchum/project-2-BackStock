b6a66276995bd5ea37065e14b7575f2a7a530429
David Hybner <dah1998@yahoo.com>
2022-10-17 14:40:14 -0400

    routes/ handlebars meet
    got the routes to display handlebar pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
 
+router.get('/', (req, res) => {
+    res.render('landingpage');
+});
+
+router.get('/login', (req, res) => {
+    // if (req.session.loggedIn) {
+    //     res.redirect('/locations');
+    //     return;
+    //   }
+    res.render('login');
+});
+router.get('/locations', (req, res) => {
+   res.render('locations')
+});
+router.get('/backstock', (req, res) => {
+    res.render('backstock')
+ });
+
+
 module.exports = router;
