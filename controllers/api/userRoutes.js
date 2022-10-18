7283724661fb5371ec61e0040c485ce2f61fb43d
PDKetchum <pxiong265@gmail.com>
2022-10-18 17:59:54 -0500

    fixed home page button

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,6 +58,7 @@ router.post("/login", async (req, res) => {
 
       res.json({ user: userData, message: "You are now logged in!" });
     });
+    console.log(req.session);
   } catch (err) {
     res.status(400).json(err);
   }
