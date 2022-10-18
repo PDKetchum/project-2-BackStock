03381f1858551bd87b187fc468dc160dc985b288
PDKetchum <pxiong265@gmail.com>
2022-10-18 00:37:38 -0500

    added session for users who signed up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -33,14 +33,16 @@ const signupFormHandler = async (event) => {
   if (name && email && password) {
     const response = await fetch("/api/user", {
       method: "POST",
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name: name, email: email, password: password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
       document.location.replace("/homepage");
+      console.log(response);
     } else {
       alert(response.statusText);
+      console.log(response);
     }
   }
 };
