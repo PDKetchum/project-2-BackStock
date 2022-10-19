6ed1434597a268dd8a291b10edabda98d36e08e2
Dymond <dymond.hoffman03@gmail.com>
2022-10-19 16:38:08 -0700

    anime.js done

--- public/javascript/landingpage.js
+++ public/javascript/landingpage.js
@@ -1,8 +1,28 @@
-// import anime.js
-// const anime = require('animejs');
+// Animation to spin image of boxes when page loads
+window.addEventListener("load", (event) => {
+  let animation = anime({
+    targets: '#boxes',
+    rotate: {
+      value: 360,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 2,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    delay: 250,
+  });
 
-var btn = document.querySelector("#btn");
+  // Animate when mouse hovers over image
+  document.querySelector('#boxes').addEventListener("mouseover", animation.restart)
+})
 
+var btn = document.querySelector("##login-btn");
+
+// Add animate.css class to login button
 btn.addEventListener("mouseover", (event) => {
   function delay(time) {
     btn.classList.add("animate__animated", "animate__pulse");
